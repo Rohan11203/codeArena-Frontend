f1b91e49e81c918544977a744a7101e6dfd68ac1
Rohan <rohanshikhare410@gmail.com>
2025-05-25 13:56:15 +0530

    minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
-import './App.css'
-import { AppRoute } from './routes/AppRoute'
-import { StoreProvider } from './ContextAPi/store/ContextProvide'
+import { useState } from "react";
+import "./App.css";
+import { AppRoute } from "./routes/AppRoute";
+import { StoreProvider } from "./ContextAPi/store/ContextProvide";
 
 function App() {
-
+  const isAuth = localStorage.getItem("isAuth");
+  if (!isAuth) {
+    localStorage.setItem("isAuth", "false");
+  }
   return (
     <>
-    <StoreProvider>
-      <AppRoute />
-    </StoreProvider>
+      <StoreProvider>
+        <AppRoute />
+      </StoreProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
